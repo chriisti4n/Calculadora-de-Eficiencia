a49764a0ba35c7ecec664fcf9f593434f0b29bdb
chriisti4n <139642038+chriisti4n@users.noreply.github.com>
2024-07-04 10:46:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function calcularEficiencia() {
-    const horasPlanejadas = document.getElementById('horasPlanejadas').value;
-    const horasConfirmadas = document.getElementById('horasConfirmadas').value;
+    const horasPlanejadas = parseFloat(document.getElementById('horasPlanejadas').value);
+    const horasConfirmadas = parseFloat(document.getElementById('horasConfirmadas').value);
 
-    if (horasPlanejadas == 0) {
-        alert("Erro: o valor de horas planejadas não pode ser zero.");
+    if (horasPlanejadas === 0 || isNaN(horasPlanejadas) || isNaN(horasConfirmadas)) {
+        alert("Erro: os valores de horas planejadas e confirmadas devem ser válidos e diferentes de zero.");
         return;
     }
 
-    const eficiencia = (horasPlanejadas / horasConfirmadas) * 100;
-    const resultadoElement = document.getElementById('resultado');
-    resultadoElement.innerText = `A eficiência é: ${eficiencia.toFixed(2)}%`;
+    const eficiencia = (horasConfirmadas / horasPlanejadas) * 100;
+    const textoResultado = document.getElementById('textoResultado');
+    const circuloElement = document.getElementById('circulo');
+
+    textoResultado.innerText = `${eficiencia.toFixed(0)}%`;
 
     // Verifica se a eficiência está entre 80% e 120%
     if (eficiencia >= 80 && eficiencia <= 120) {
-        resultadoElement.style.color = 'green'; // Se estiver dentro desse intervalo, a cor é verde
+        circuloElement.style.borderColor = 'green'; // Se estiver dentro desse intervalo, a cor é verde
     } else {
-        resultadoElement.style.color = 'red'; // Se não estiver dentro desse intervalo, a cor é vermelha
+        circuloElement.style.borderColor = 'red'; // Se não estiver dentro desse intervalo, a cor é vermelha
     }
 }
-
